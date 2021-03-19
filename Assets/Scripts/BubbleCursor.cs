9bf6129d4c7098e13758655f04c66118fb4648f9
Ivan Hernandez <hernandezivancs@gmail.com>
2021-03-19 15:48:57 -0700

    Added distractors that control Goal's effective width

--- Assets/Scripts/BubbleCursor.cs
+++ Assets/Scripts/BubbleCursor.cs
@@ -12,6 +12,8 @@ public class BubbleCursor : MonoBehaviour
     public Texture2D hiddenCursor;
     public Text text;
 
+    Vector2 cameraSize;
+    Vector2 lastGoalPos;
     Collider2D targetCollider;
     bool collided = false;
 
@@ -32,6 +34,10 @@ public class BubbleCursor : MonoBehaviour
 
 
     void Start() {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        cameraSize = new Vector2((cameraHeight * screenAspect) / 2, cameraHeight / 2);
+
         StartTrial();
 
         // Hide Cursor
@@ -45,6 +51,10 @@ public class BubbleCursor : MonoBehaviour
 
         // Apply Bubble Cursor Algorithm
         BubbleAlgorithm();
+
+        if (GoalTarget) {
+            Debug.DrawLine(lastGoalPos, GoalTarget.transform.position, Color.blue);
+        }
 
         // Check if goal target is selected
         TargetSelection();
@@ -122,6 +132,7 @@ public class BubbleCursor : MonoBehaviour
             // If Successful Click
             if ((GoalTarget != null && targetCollider.gameObject.Equals(GoalTarget))) {
                 text.gameObject.SetActive(false);
+                lastGoalPos = GoalTarget.transform.position;
 
                 ClearTargets();
 
@@ -139,33 +150,60 @@ public class BubbleCursor : MonoBehaviour
 
     void SpawnTargets() {
         // Get W and D values every click
+        // currentDist = A[clicks % 3];
         currentRadius = W[clicks % 3] / 2.0f;
-        currentDensity = (int)(D[(int)(clicks / 3)] * numDistractors);
+        currentDensity = (int)(D[(int)(clicks / 3)] * numDistractors) + 5;
 
         // New Targets
         Targets = new GameObject[currentDensity];
 
-        for (int i = 0; i < currentDensity; i++) {
+        SpawnGoal();
+        for (int i = 5; i < currentDensity; i++) {
             GameObject go;
             do {
                 float ScreenOffset = currentRadius * 100;
                 Vector2 randomPosition = new Vector2(Random.Range(0 + ScreenOffset, Screen.width - ScreenOffset), Random.Range(0 + ScreenOffset, Screen.height - ScreenOffset));
                 Vector2 targetPosition = Camera.main.ScreenToWorldPoint(randomPosition);
 
-                if (i == 0) {
-                    go = Instantiate(GoalRef, targetPosition, Quaternion.identity) as GameObject;
-                    GoalTarget = go;
-                }
-                else {
-                    go = Instantiate(DistractorRef, targetPosition, Quaternion.identity) as GameObject;
-                }
+                go = Instantiate(DistractorRef, targetPosition, Quaternion.identity) as GameObject;
 
             } while (!checkOverlap(go, i));
 
             go.transform.localScale = new Vector2(currentRadius, currentRadius);
             Targets[i] = go;
         }
+
+        
     }
+
+    void SpawnGoal() {
+
+        // Create Goal Target
+        do {
+            float angle = Random.Range(0, Mathf.PI * 2);
+            Vector2 pos2d = new Vector2(Mathf.Sin(angle) * 6.0f, Mathf.Cos(angle) * 6.0f) + lastGoalPos;
+            GoalTarget = Instantiate(GoalRef, pos2d, Quaternion.identity) as GameObject;
+
+        } while (checkBounds(GoalTarget));
+
+        GoalTarget.transform.localScale = new Vector2(currentRadius, currentRadius);
+        Targets[0] = GoalTarget;
+
+        // Four Distractors to control Goal Target's Effective Width
+        for (int i = 1; i < 5; i++) {
+            Vector2 dir = lastGoalPos - (Vector2)GoalTarget.transform.position;
+            float sign = (GoalTarget.transform.position.y < lastGoalPos.y) ? -1.0f : 1.0f;
+            float dist = (currentRadius == W[0] / 2) ? 1.0f : 2.0f;
+
+            float angle = Mathf.Deg2Rad * (Vector2.Angle(Vector2.right, dir) * sign + (90 * i));
+            Vector2 pos2d = new Vector2(Mathf.Sin(angle) * dist, Mathf.Cos(angle) * dist) + (Vector2)GoalTarget.transform.position;
+            GameObject go = Instantiate(DistractorRef, pos2d, Quaternion.identity) as GameObject;
+            go.transform.localScale = new Vector2(currentRadius, currentRadius);
+            Targets[i] = go;
+        }
+    }
+
+
     // Reset Screen
     void ClearTargets() {
         for (int i = 0; i < currentDensity; i++) {
@@ -179,12 +217,22 @@ public class BubbleCursor : MonoBehaviour
     private bool checkOverlap(GameObject obj, int maxIndex) {
         for (int i = 0; i < maxIndex; i++) {
             Vector2 distVec = obj.transform.position - Targets[i].transform.position;
-            if (distVec.magnitude < currentRadius * 2) {
+            if (distVec.magnitude < (currentRadius * 2) + 0.5f) {
                 Destroy(obj);
                 return false;
             }
         }
         return true;
+    }
+
+    private bool checkBounds(GameObject obj) {
+        Vector2 pos = obj.transform.position;
+        if (pos.x < (-cameraSize.x + currentRadius + 2.0f) || pos.x > (cameraSize.x - currentRadius - 2.0f) ||
+            pos.y < (-cameraSize.y + currentRadius + 2.0f) || pos.y > (cameraSize.y - currentRadius - 2.0f)) {
+            Destroy(obj);
+            return true;
+        }
+        return false;
     }
 
 
