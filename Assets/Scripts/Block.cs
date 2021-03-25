315c5ecef3b6d7ef5060d9c127445e394a7c98b8
AlexWolski <alex.theinfinite@gmail.com>
2021-03-25 02:20:09 -0700

    Modified Block.cs to follow the IEnumerator rules

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -33,59 +33,27 @@ public class Block : IEnumerator, IEnumerable
 
     private static System.Random rng = new System.Random();
 
-    private int numTrials = -1;
+    private bool initialized = false;
+    private int numTrials;
+    // The default trial is one before the first element
+    private int currTrial = -1;
     private TrialVars[] combinations;
     private int[] randomIndices;
 
     public int NumDistractors { get { return numDistractors; } }
     public int ClicksPerTrial { get { return clicksPerTrial; } }
 
-    public int NumTrials
+    private void initialize()
     {
-        get
-        {
-            if (numTrials < 0)
-                numTrials = A.Length * D.Length * W.Length;
-
-            return numTrials;
-        }
-
-        private set { numTrials = value; }
+        numTrials = A.Length * D.Length * W.Length;
+        genCombinations();
+        genRandomIndices();
     }
-
-    public TrialVars[] Combinations
-    {
-        get
-        {
-            if (combinations == null)
-                genCombinations();
-
-            return combinations;
-        }
-
-        private set { combinations = value; }
-    }
-
-    public int[] RandomIndices
-    {
-        get
-        {
-            if (randomIndices == null)
-                genRandomIndices();
-
-            return randomIndices;
-        }
-
-        private set { randomIndices = value; }
-    }
-
-    public int CurrTrial { get; private set; }
-    public TrialVars CurrTrialVars { get; private set; }
 
     private void genCombinations()
     {
         // Instantiate an array to contain all the combinations
-        Combinations = new TrialVars[NumTrials];
+        combinations = new TrialVars[numTrials];
 
         int combIndex = 0;
 
@@ -93,22 +61,22 @@ public class Block : IEnumerator, IEnumerable
         for (int AIndex = 0; AIndex < A.Length; AIndex++)
             for (int DIndex = 0; DIndex < D.Length; DIndex++)
                 for (int WIndex = 0; WIndex < W.Length; WIndex++)
-                    Combinations[combIndex++] = new TrialVars(A[AIndex], D[DIndex], W[WIndex]);
+                    combinations[combIndex++] = new TrialVars(A[AIndex], D[DIndex], W[WIndex]);
     }
 
     private void genRandomIndices()
     {
         // Generate an array containing each index of the combinations array
-        RandomIndices = Enumerable.Range(0, NumTrials).ToArray();
+        randomIndices = Enumerable.Range(0, numTrials).ToArray();
 
         // Randomize the array using Fisher-Yates shuffle
-        for (int index1 = RandomIndices.Length - 1; index1 > 0; index1--)
+        for (int index1 = randomIndices.Length - 1; index1 > 0; index1--)
         {
             index1--;
             int index2 = rng.Next(index1 + 1);
-            int tempValue = RandomIndices[index2];
-            RandomIndices[index2] = RandomIndices[index1];
-            RandomIndices[index1] = tempValue;
+            int tempValue = randomIndices[index2];
+            randomIndices[index2] = randomIndices[index1];
+            randomIndices[index1] = tempValue;
         }
     }
 
@@ -121,19 +89,25 @@ public class Block : IEnumerator, IEnumerable
     // IEnumerable
     public void Reset()
     {
-        CurrTrial = 0;
+        currTrial = -1;
         genRandomIndices();
     }
 
     // IEnumerator
     public bool MoveNext()
     {
+        if (!initialized)
+        {
+            initialize();
+            initialized = true;
+        }
+
         // Return false if there is no next trial
-        if (CurrTrial >= NumTrials)
+        if (currTrial >= numTrials)
             return false;
 
         // Move to the next trial
-        CurrTrial++;
+        currTrial++;
 
         return true;
     }
@@ -143,8 +117,13 @@ public class Block : IEnumerator, IEnumerable
     {
         get
         {
-            int combIndex = RandomIndices[CurrTrial];
-            return Combinations[combIndex];
+            if (!initialized)
+                throw new InvalidOperationException("Enumeration has not started. Call MoveNext.");
+            else if (currTrial >= numTrials)
+                throw new InvalidOperationException("Enumeration already finished.");
+
+            int combIndex = randomIndices[currTrial];
+            return combinations[combIndex];
         }
     }
 }
