a2229d06febb822b3379ae47e02f910847bf6564
AlexWolski <alex.theinfinite@gmail.com>
2021-03-25 12:28:42 -0700

    Rearranged target spawning methods in ExperimentManager

--- Assets/Scripts/ExperimentManager.cs
+++ Assets/Scripts/ExperimentManager.cs
@@ -37,10 +37,10 @@ public class ExperimentManager : MonoBehaviour
     public GameObject GoalObject { get; private set; }
     public List<GameObject> Targets { get; private set; }
 
-    private IEnumerator ITrainingVars;
-    private IEnumerator IExperimentVars;
-    private IEnumerator ICurrentVars;
-    private int currentTrial = 0;
+    private IEnumerator ITrainingTrials;
+    private IEnumerator IExperimentTrials;
+    private IEnumerator ICurrentTrial;
+    private int currentClick = 0;
     private Vector2 lastGoalPos;
     Vector2 cameraSize;
 
@@ -60,45 +60,37 @@ public class ExperimentManager : MonoBehaviour
         bubbleCursor.SetActive(true);
         //pointCursor.SetActive(true);
         Targets = new List<GameObject>();
-        ITrainingVars = trainingBlock.GetEnumerator();
-        IExperimentVars = experimentBlock.GetEnumerator();
-        ICurrentVars = IExperimentVars;
+        ITrainingTrials = trainingBlock.GetEnumerator();
+        IExperimentTrials = experimentBlock.GetEnumerator();
+        ICurrentTrial = IExperimentTrials;
         startTrial();
     }
 
     private void startTrial()
     {
+        clearTargets();
+
         Vector2 targetPosition = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width / 2.0f, Screen.height / 2.0f));
-        GoalObject = spawnTarget(goalPrefab, targetPosition, targetRadius);
-        ICurrentVars.MoveNext();
+        GoalObject = InstantiateTarget(goalPrefab, targetPosition, targetRadius);
+        lastGoalPos = GoalObject.transform.position;
+        
+        ICurrentTrial.Reset();
+        ICurrentTrial.MoveNext();
 
         trialStartText.gameObject.SetActive(true);
         trialStartText.text = "Trial " + 1 + " \nClick the target to start";
     }
 
-    void SpawnTargets(TrialVars variables, int numDistractors)
+    private void spawnTargets(TrialVars variables, int numDistractors)
     {
         clearTargets();
-        Targets = new List<GameObject>(numDistractors + 5);
-
         SpawnGoal(variables);
-
-        for (int i = 0; i < numDistractors; i++)
-        {
-            GameObject target = spawnTarget(distractorPrefab, new Vector2(0.0f, 0.0f), targetRadius);
-            float ScreenOffset = targetRadius * 100.0f;
-
-            do
-            {
-                Vector2 randomPosition = new Vector2(Random.Range(0 + ScreenOffset, Screen.width - ScreenOffset), Random.Range(0 + ScreenOffset, Screen.height - ScreenOffset));
-                target.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(randomPosition);
-            } while (!checkOverlap(target));
-        }
+        SpawnDistractors(variables, numDistractors);
     }
 
-    void SpawnGoal(TrialVars variables)
+    private void SpawnGoal(TrialVars variables)
     {
-        GoalObject = spawnTarget(goalPrefab, new Vector2(0.0f, 0.0f), targetRadius);
+        GoalObject = InstantiateTarget(goalPrefab, new Vector2(0.0f, 0.0f), targetRadius);
 
         // Set the location of the goal target
         do
@@ -106,8 +98,7 @@ public class ExperimentManager : MonoBehaviour
             float angle = Random.Range(0, Mathf.PI * 2.0f);
             GoalObject.transform.position = new Vector2(Mathf.Sin(angle) * 6.0f, Mathf.Cos(angle) * 6.0f) + lastGoalPos;
 
-        } while (checkBounds(GoalObject));
-
+        } while (!checkInBounds(GoalObject));
 
         // Four Distractors to control Goal Target's Effective Width
         for (int i = 0; i < 4; i++)
@@ -118,11 +109,28 @@ public class ExperimentManager : MonoBehaviour
 
             float angle = Mathf.Deg2Rad * (Vector2.Angle(Vector2.right, dir) * sign + (90 * i));
             Vector2 pos2d = new Vector2(Mathf.Sin(angle) * dist, Mathf.Cos(angle) * dist) + (Vector2)GoalObject.transform.position;
-            spawnTarget(distractorPrefab, pos2d, targetRadius);
+            InstantiateTarget(distractorPrefab, pos2d, targetRadius);
+        }
+
+        lastGoalPos = GoalObject.transform.position;
+    }
+
+    private void SpawnDistractors(TrialVars variables, int numDistractors)
+    {
+        for (int i = 0; i < numDistractors; i++)
+        {
+            GameObject target = InstantiateTarget(distractorPrefab, new Vector2(0.0f, 0.0f), targetRadius);
+            float ScreenOffset = targetRadius * 100.0f;
+
+            do
+            {
+                Vector2 randomPosition = new Vector2(Random.Range(0 + ScreenOffset, Screen.width - ScreenOffset), Random.Range(0 + ScreenOffset, Screen.height - ScreenOffset));
+                target.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(randomPosition);
+            } while (checkOverlap(target));
         }
     }
 
-    private GameObject spawnTarget(GameObject targetPrefab, Vector2 position, float radius)
+    private GameObject InstantiateTarget(GameObject targetPrefab, Vector2 position, float radius)
     {
         GameObject target = Instantiate(targetPrefab, position, Quaternion.identity);
         target.transform.SetParent(targetsRoot.transform);
@@ -138,6 +146,7 @@ public class ExperimentManager : MonoBehaviour
             Destroy(target);
 
         GoalObject = null;
+        Targets = new List<GameObject>();
     }
 
     private bool checkOverlap(GameObject target1)
@@ -152,40 +161,40 @@ public class ExperimentManager : MonoBehaviour
             float radius2 = target2.GetComponent<Target>().Radius;
 
             if (distVec.magnitude < (radius1 + radius2) + targetMargin)
-                return false;
+                return true;
         }
 
-        return true;
+        return false;
     }
 
-    private bool checkBounds(GameObject target)
+    private bool checkInBounds(GameObject target)
     {
         Vector2 pos = target.transform.position;
         float radius = target.GetComponent<Target>().Radius;
 
         if (pos.x < (-cameraSize.x + radius + targetMargin) || pos.x > (cameraSize.x - radius - targetMargin) ||
             pos.y < (-cameraSize.y + radius + targetMargin) || pos.y > (cameraSize.y - radius - targetMargin))
-            return true;
+            return false;
 
-        return false;
+        return true;
     }
 
     public void targetHit(GameObject hitObject)
     {
         if (hitObject == GoalObject)
         {
-            if (currentTrial >= experimentBlock.ClicksPerTrial)
+            if (currentClick >= experimentBlock.ClicksPerTrial)
             {
-                if (ICurrentVars.MoveNext())
+                if (ICurrentTrial.MoveNext())
                     startTrial();
                 else
-                    ICurrentVars.Reset();
+                    ICurrentTrial.Reset();
 
-                currentTrial = 0;
+                currentClick = 0;
             }
 
-            SpawnTargets((TrialVars)ICurrentVars.Current, 25);
-            currentTrial++;
+            spawnTargets((TrialVars)ICurrentTrial.Current, 25);
+            currentClick++;
         }
         else
         {
