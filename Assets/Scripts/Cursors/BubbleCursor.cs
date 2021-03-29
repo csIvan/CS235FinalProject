73f626666a2db66dddd021cde6b4344246cd6561
Ivan Hernandez <hernandezivancs@gmail.com>
2021-03-28 23:37:18 -0700

    Added bubble animation

--- Assets/Scripts/Cursors/BubbleCursor.cs
+++ Assets/Scripts/Cursors/BubbleCursor.cs
@@ -10,10 +10,17 @@ public class BubbleCursor : MonoBehaviour
 
     private float bubbleRadius = -1.0f;
     private GameObject selectedObject = null;
+    private GameObject bubble = null;
 
     private float movementTime = 0;
     private float timer = 5.0f;
     private bool resetTime = false;
+
+    private Vector2 bubbleStartPos;
+    private float bubbleStartDist = 0.4f;
+    private float bubbleMaxScale = 1.3f;
+    private bool startBubble = false;
+    private bool expandBubble = false;
 
     // Update is called once per frame
     void Update()
@@ -31,6 +38,8 @@ public class BubbleCursor : MonoBehaviour
 
         // Apply Bubble Cursor Algorithm
         BubbleAlgorithm();
+        AnimateBubble();
+
 
         if (Input.GetMouseButtonDown(0)) {
             // Reset movement time if selected object is a goal
@@ -90,15 +99,51 @@ public class BubbleCursor : MonoBehaviour
     }
 
 
+    private void AnimateBubble() {
+        // Initialize bubble position and scale
+        if (startBubble) {
+            bubble.transform.localScale = Vector2.zero;
+
+            Vector2 dir = transform.localPosition - selectedObject.transform.localPosition;
+            bubble.transform.localPosition = dir.normalized * bubbleStartDist;
+
+            startBubble = false;
+            expandBubble = true;
+        }
+
+        // Animate bubble into position and scale it up
+        if (expandBubble) {
+            if (selectedObject.transform.position != bubble.transform.position) {
+                bubble.transform.localPosition = Vector3.MoveTowards(bubble.transform.localPosition, Vector2.zero, 2 * Time.deltaTime);
+
+                float dist = (bubble.transform.localPosition - Vector3.zero).magnitude;
+                bubble.transform.localScale = new Vector2(bubbleMaxScale - (dist / bubbleStartDist), bubbleMaxScale - (dist / bubbleStartDist));
+            }
+        }
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Target"))
         {
             selectedObject = collision.gameObject;
             collision.gameObject.GetComponent<Target>().Selected = true;
+            bubble = selectedObject.transform.Find("Bubble").gameObject;
+            startBubble = true;
         }
     }
 
+    private void OnTriggerStay2D(Collider2D collision) 
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Target")) 
+        {
+            selectedObject = collision.gameObject;
+            collision.gameObject.GetComponent<Target>().Selected = true;
+            bubble = selectedObject.transform.Find("Bubble").gameObject;
+            expandBubble = true;
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -106,6 +151,8 @@ public class BubbleCursor : MonoBehaviour
         {
             collision.gameObject.GetComponent<Target>().Selected = false;
             selectedObject = null;
+            bubble = null;
+            expandBubble = false;
         }
     }
 }
